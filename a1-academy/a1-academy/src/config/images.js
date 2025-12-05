b6e28899d263a5a6f565e55aad750ad4f6d893ff
Sahil Mandre <sahilmandre@gmail.com>
2025-12-05 14:31:25 +0530

    Added the logo

--- a1-academy/a1-academy/src/config/images.js
+++ a1-academy/a1-academy/src/config/images.js
@@ -1,27 +1,23 @@
 import admissionImage from '../assets/admission.png';
 import directorImage from '../assets/director.png';
+import logoImage from "../assets/logo.png";
 import hof1 from '../assets/Hof1.png';
 import hof2 from '../assets/Hof2.png';
 import hof3 from '../assets/Hof3.png';
 import hof4 from '../assets/Hof4.png';
 
 export const IMAGES = {
-  logo: "image_f6a565.png",
+  logo: logoImage,
   // logo: "https://cdn-icons-png.flaticon.com/512/3665/3665939.png", // Fallback
-  
+
   classroom: "image_f6a585.jpg",
   // classroom: "https://images.unsplash.com/photo-1577896334614-201b31d50dc5?auto=format&fit=crop&q=80&w=1920", // Fallback
-  
+
   director: directorImage,
   // director: "https://images.unsplash.com/photo-1524178232363-1fb2b075b955?auto=format&fit=crop&q=80&w=1000", // Fallback
-  
+
   admission: admissionImage,
   // admission: "https://images.unsplash.com/photo-1523050854058-8df90110c9f1?auto=format&fit=crop&q=80&w=1000", // Fallback
-  
-  results: [
-    hof1,
-    hof2,
-    hof3,
-    hof4
-  ]
+
+  results: [hof1, hof2, hof3, hof4],
 };
